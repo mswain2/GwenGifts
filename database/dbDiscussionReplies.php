a8b5d6995bc6ad811d97d9f90bb805fdb148753d
Saumik Mullah <saumikmu@gmail.com>
2026-03-18 11:50:33 -0400

    Board Discussion reply fix

--- database/dbDiscussionReplies.php
+++ database/dbDiscussionReplies.php
@@ -63,10 +63,11 @@ function add_reply_to_reply($discussion, $replyID, $reply_body, $author_id, $par
     $reply_body = mysqli_real_escape_string($con, $reply_body);
     $author_id = mysqli_real_escape_string($con, $author_id);
     $parent_reply_id = mysqli_real_escape_string($con, $parent_reply_id);
+    $category = mysqli_real_escape_string($con, $discussion['category'] ?? 'general');
     $created_at = date("Y-m-d-H:i");
 
-    $query = "INSERT INTO discussion_replies (user_reply_id, author_id, discussion_title, reply_body, parent_reply_id, created_at) 
-              VALUES ('$replyID', '$author_id', '$discussion_title', '$reply_body', '$parent_reply_id', '$created_at')";
+    $query = "INSERT INTO discussion_replies (user_reply_id, author_id, discussion_title, reply_body, parent_reply_id, created_at, category) 
+              VALUES ('$replyID', '$author_id', '$discussion_title', '$reply_body', '$parent_reply_id', '$created_at', '$category')";
 
     $result = mysqli_query($con, $query);
     mysqli_close($con);
