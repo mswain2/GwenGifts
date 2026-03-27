80aa8351ceb5de319194c131257d9cc4b1e9b601
oliviab <ofbuchanan@icloud.com>
2026-03-27 00:00:18 -0400

    Added images/pencil-solid-full.svg and finished fixing nav_admin.php

--- partials/nav_admin.php
+++ partials/nav_admin.php
@@ -17,7 +17,7 @@
                         <span>Create Event</span>
                     </div>
                     </a>
-                    <a href="calendar.php" style="text-decoration: none;">
+                    <a href="viewAllEvents.php" style="text-decoration: none;">
                     <div class="in-nav">
                         <img src="images/list-solid.svg">
                         <span>View Events</span>
@@ -37,7 +37,7 @@
                     </a>
                     <a href="adminViewingEvents.php" style="text-decoration: none;">
                     <div class="in-nav">
-                        <img src="images/list-solid.svg">
+                        <img src="images/pencil-solid-full.svg">
                         <span>Edit Event</span>
                     </div>
                     </a>
@@ -73,13 +73,14 @@
     <!-- Right Section: Date & Icon -->
     
     <div class="right-section">
-        <!--<a href="calendar.php">
+        <a href="calendar.php">
             <div class="icon-butt">
                 <svg width="30" height="30" viewBox="0 0 24 24" fill="var(--main-color)" xmlns="http://www.w3.org/2000/svg">
                     <path d="M3 4C3 3.44772 3.44772 3 4 3H6V2C6 1.44772 6.44772 1 7 1C7.55228 1 8 1.44772 8 2V3H16V2C16 1.44772 16.4477 1 17 1C17.5523 1 18 1.44772 18 2V3H20C20.5523 3 21 3.44772 21 4V21C21 21.5523 20.5523 22 20 22H4C3.44772 22 3 21.5523 3 21V4ZM5 5V20H19V5H5ZM7 10H9V12H7V10ZM11 10H13V12H11V10ZM15 10H17V12H15V10ZM7 14H9V16H7V14ZM11 14H13V16H11V14ZM15 14H17V16H15V14Z"/>
                 </svg>
             </div>
-        </a>-->
+        </a>
+        <div class="date-box"></div>
         <div class="nav-links">
             <div class="nav-item">
                 <div class="icon">
