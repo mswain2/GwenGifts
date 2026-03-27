1198918b488a66561f746cd8be9ca3fdb503aae4
oliviab <ofbuchanan@icloud.com>
2026-03-26 22:24:58 -0400

    Olivia moved header.php back into main project root, wrote partials/footer.php and partials/toasts.php

--- index.php
+++ index.php
@@ -64,21 +64,7 @@ $notRoot = $person->get_id() != 'vmsroot';
             <h2><b>Welcome <?php echo $person->get_first_name() ?>!</b> Let's get started.</h2>
         </div>
 
-        <?php if (isset($_GET['pcSuccess'])): ?>
-            <div class="happy-toast">Password changed successfully!</div>
-        <?php elseif (isset($_GET['deleteService'])): ?>
-            <div class="happy-toast">Service successfully removed!</div>
-        <?php elseif (isset($_GET['serviceAdded'])): ?>
-            <div class="happy-toast">Service successfully added!</div>
-        <?php elseif (isset($_GET['animalRemoved'])): ?>
-            <div class="happy-toast">Animal successfully removed!</div>
-        <?php elseif (isset($_GET['locationAdded'])): ?>
-            <div class="happy-toast">Location successfully added!</div>
-        <?php elseif (isset($_GET['deleteLocation'])): ?>
-            <div class="happy-toast">Location successfully removed!</div>
-        <?php elseif (isset($_GET['registerSuccess'])): ?>
-            <div class="happy-toast">Volunteer registered successfully!</div>
-        <?php endif ?>
+        <?php require 'partials/toasts.php'; ?>
 
         <div class="full-width-bar">
             <div class="content-box">
@@ -274,43 +260,7 @@ $notRoot = $person->get_id() != 'vmsroot';
 
         </div>
 
-        <div style="width: 90%;
-            height: 100%;
-            outline: 1px #828282 solid;
-            outline-offset: -0.5px;
-            margin: 70px auto;
-            padding: 1px 0;">
-        </div>
-
-        <footer class="footer" style="margin-top: 100px;">
-            <!-- Left Side: Logo & Socials -->
-            <div class="footer-left">
-                <img src="images/logo.png" alt="Logo" class="footer-logo">
-                <div class="social-icons">
-                    <a href="#"><i class="fab fa-facebook"></i></a>
-                    <a href="#"><i class="fab fa-twitter"></i></a>
-                    <a href="#"><i class="fab fa-instagram"></i></a>
-                    <a href="#"><i class="fab fa-linkedin"></i></a>
-                </div>
-            </div>
-
-            <!-- Right Side: Page Links -->
-            <div class="footer-right">
-                <div class="footer-section">
-                    <div class="footer-topic">Connect</div>
-                    <a href="https://www.facebook.com/gwynethsgift">Facebook</a>
-                    <a href="https://www.instagram.com/gwynethsgift/">Instagram</a>
-                    <a href="https://gwynethsgift.org/">Main Website</a>
-                </div>
-                <div class="footer-section">
-                    <div class="footer-topic">Contact Us</div>
-                    <a href="https://gwynethsgift.org/contact-us/">Send Us An Email</a>
-                </div>
-            </div>
-        </footer>
-
-        <!-- Font Awesome for Icons -->
-        <script src="https://kit.fontawesome.com/yourkit.js" crossorigin="anonymous"></script>
+        <?php require 'partials/footer.php'; ?>
 
     </body>
 <?php endif ?>
@@ -325,9 +275,7 @@ $notRoot = $person->get_id() != 'vmsroot';
             <h2><b>Welcome <?php echo $person->get_first_name() ?>!</b> Let's get started.</h2>
         </div>
 
-        <?php if (isset($_GET['pcSuccess'])): ?>
-            <div class="happy-toast">Password changed successfully!</div>
-        <?php endif ?>
+        <?php require 'partials/toasts.php'; ?>
 
         <div class="full-width-bar">
             <div class="content-box">
@@ -452,43 +400,7 @@ $notRoot = $person->get_id() != 'vmsroot';
 
         </div>
 
-        <div style="width: 90%;
-            height: 100%;
-            outline: 1px #828282 solid;
-            outline-offset: -0.5px;
-            margin: 70px auto;
-            padding: 1px 0;">
-        </div>
-
-        <footer class="footer" style="margin-top: 100px;">
-            <!-- Left Side: Logo & Socials -->
-            <div class="footer-left">
-                <img src="images/logo.png" alt="Logo" class="footer-logo">
-                <div class="social-icons">
-                    <a href="#"><i class="fab fa-facebook"></i></a>
-                    <a href="#"><i class="fab fa-twitter"></i></a>
-                    <a href="#"><i class="fab fa-instagram"></i></a>
-                    <a href="#"><i class="fab fa-linkedin"></i></a>
-                </div>
-            </div>
-
-            <!-- Right Side: Page Links -->
-            <div class="footer-right">
-                <div class="footer-section">
-                    <div class="footer-topic">Connect</div>
-                    <a href="https://www.facebook.com/gwynethsgift">Facebook</a>
-                    <a href="https://www.instagram.com/gwynethsgift/">Instagram</a>
-                    <a href="https://gwynethsgift.org/">Main Website</a>
-                </div>
-                <div class="footer-section">
-                    <div class="footer-topic">Contact Us</div>
-                    <a href="https://gwynethsgift.org/contact-us/">Send Us An Email</a>
-                </div>
-            </div>
-        </footer>
-
-        <!-- Font Awesome for Icons -->
-        <script src="https://kit.fontawesome.com/yourkit.js" crossorigin="anonymous"></script>
+        <?php require 'partials/footer.php'; ?>
 
     </body>
 <?php endif ?>
