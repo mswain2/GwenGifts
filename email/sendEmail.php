f7e02f37eb876af936e41a98679aa33a117ee7e7
Kenzie <148883845+mswain2@users.noreply.github.com>
2026-04-09 10:44:28 -0400

    Got email working. Users can create emails and send them immediately. THey can also save the email as a draft and edit it as a draft. The scheduled emails is technically working but will need some stuff done with SiteGround first.

--- email/sendEmail.php
+++ email/sendEmail.php
@@ -7,7 +7,8 @@ ini_set('display_errors', 1);
 // -------------------------------------------------------
 // Load .env file (must be in parent directory)
 // -------------------------------------------------------
-$envPath = "/home/customer/www/jenniferp217.sg-host.com/public_html/email/.env";
+//$envPath = "/home/customer/www/jenniferp232.sg-host.com/public_html/email/.env";
+$envPath = __DIR__ . '/email/.env';
 if (!file_exists($envPath)) {
     echo json_encode(["success" => false, "error" => ".env file not found"]);
     exit;
