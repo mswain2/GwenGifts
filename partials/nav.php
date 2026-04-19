3dd331c23acc665a489aac2a249fd80404c4c9b6
oliviab <ofbuchanan@icloud.com>
2026-04-18 22:55:12 -0400

    Cleaned up the code in nav.php to be more consistent, added a css class for profile icon's many properties, added icons to dropdown for the profile icon, and changed the logo in the top right to be the full logo

--- partials/nav.php
+++ partials/nav.php
@@ -1,12 +1,12 @@
 <div class="navbar">
     <!-- Left Section: Logo & Nav Links -->
     <div class="left-section">
-        <div class="logo-container">
-            <a href="index.php"><img src="images/cropped-logo.png" alt="Logo"></a>
+        <div class="nav-logo">
+            <a href="index.php"><img src="images/gwynethsGiftLogo.png" alt="Logo"></a>
         </div>
         <div class="nav-links">
             <div class="nav-item"><a href="index.php">Home</a></div>
-            <div class="nav-item">My Events <span class="dropdown-arrow">&#9660</span>
+            <div class="nav-item">My Events <span class="dropdown-arrow">&#9660;</span>
                 <div class="dropdown">
                     <a href="viewAllEvents.php" style="text-decoration: none;">
                     <div class="in-nav">
@@ -37,7 +37,7 @@
 
     <!-- Right Section: Date & Icon -->
     <div class="right-section">
-        <a href="calendar.php">
+        <a href="calendar.php" title="Calendar">
             <div class="icon-butt">
                 <svg width="30" height="30" viewBox="0 0 24 24" fill="#C9AB81" xmlns="http://www.w3.org/2000/svg">
                     <path d="M3 4C3 3.44772 3.44772 3 4 3H6V2C6 1.44772 6.44772 1 7 1C7.55228 1 8 1.44772 8 2V3H16V2C16 1.44772 16.4477 1 17 1C17.5523 1 18 1.44772 18 2V3H20C20.5523 3 21 3.44772 21 4V21C21 21.5523 20.5523 22 20 22H4C3.44772 22 3 21.5523 3 21V4ZM5 5V20H19V5H5ZM7 10H9V12H7V10ZM11 10H13V12H11V10ZM15 10H17V12H15V10ZM7 14H9V16H7V14ZM11 14H13V16H11V14ZM15 14H17V16H15V14Z"/>
@@ -55,12 +55,32 @@
         <div class="nav-links">
             <div class="nav-item" style="outline:none;">
                 <div class="icon">
-                    <img src="<?= $_pfp_escaped ?>" alt="User Icon" class="icon-img in-nav-img" style="width:49px; height:49px; min-width:49px; min-height:49px; max-width:49px; max-height:49px; object-fit:cover; border-radius:50%;">
+                    <img src="<?= $_pfp_escaped ?>" alt="User Icon" class="icon-img in-nav-img profile-icon">
                     <div class="dropdown">
-                        <a href="viewProfile.php" style="text-decoration: none;"><div>View Profile</div></a>
-                        <a href="editProfile.php" style="text-decoration: none;"><div>Edit Profile</div></a>
-                        <a href="changePassword.php" style="text-decoration: none;"><div>Change Password</div></a>
-                        <a href="logout.php" style="text-decoration: none;"><div>Log Out</div></a>
+                        <a href="viewProfile.php" style="text-decoration: none;">
+                        <div class="in-nav">
+                            <img src="images/view-profile-solid.svg">
+                            <span>View Profile</span>
+                        </div>
+                        </a>
+                        <a href="editProfile.php" style="text-decoration: none;">
+                        <div class="in-nav">
+                            <img src="images/pencil-solid-full.svg">
+                            <span>Edit Profile</span>
+                        </div>
+                        </a>
+                        <a href="changePassword.php" style="text-decoration: none;">
+                        <div class="in-nav">
+                            <img src="images/change-password.svg">
+                            <span>Change Password</span>
+                        </div>
+                        </a>
+                        <a href="logout.php" style="text-decoration: none;">
+                        <div class="in-nav">
+                            <img src="images/logout.svg">
+                            <span>Log Out</span>
+                        </div>
+                        </a>
                     </div>
                 </div>
             </div>
