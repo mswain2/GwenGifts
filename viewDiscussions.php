8c08e5e636f2d1e979cc1f5e0b3a4d599267d26b
Wise Mystical Tree <emcgowan@umw.edu>
2026-04-25 19:28:41 -0400

    Eric removed discussion management button

--- viewDiscussions.php
+++ viewDiscussions.php
@@ -124,9 +124,6 @@ $discussions = get_all_discussions();
     <div class="text-center mt-6">
         <a href="index.php" class="return-button">Return to Dashboard</a>
     </div>
-    <div class="text-center mt-6">
-        <a href="discussionMain.php" class="return-button">Back to Discussions Management</a>
-    </div>
     <div class="info-section">
         <div class="blue-div"></div>
     </div>
