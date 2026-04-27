66aacdc47e857cf6a8377dbc6d43fb92076f9337
oliviab <ofbuchanan@icloud.com>
2026-04-27 00:46:15 -0400

    Created backup database for end of sprint 8. Deleted floating sql change files and moved unused sql files to unused folder.

--- sql/forcepasswordchange.sql
+++
@@ -1,5 +0,0 @@
--- Adds force_password_change column to dbpersons table
--- Run this if database already exists and you need the new column
--- Safe to run multiple times (IF NOT EXISTS)
-
-ALTER TABLE dbpersons ADD COLUMN IF NOT EXISTS force_password_change TINYINT(1) DEFAULT 0;
